06b42b7f68a520a3b4f4a44d104a1655bceb6b93
Abdulrahman Jamiu <139684072+papi-jamie@users.noreply.github.com>
2024-03-26 00:24:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function($) {
     $('.message a').click(function(){
         $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
     });
