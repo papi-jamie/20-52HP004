295cdfa70a9b1f3e53b7e2712a85a335dc1ab6ef
Abdulrahman Jamiu <139684072+papi-jamie@users.noreply.github.com>
2024-03-26 01:17:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,18 @@ var isHidden = false;
 function toggleVisibility() {
     if (isHidden) {
         $('.text-message').css('opacity', 1);
-        $('button').text('Hide the text above');
+        $('#toggleButton').text('Hide the text above');
     } else {
         $('.text-message').css('opacity', 0);
-        $('button').text('Show the text above');
+        $('#toggleButton').text('Show the text above');
     }
     isHidden = !isHidden;
 }
 
-$('button').on('click', function() {
-    toggleVisibility();
+$(document).ready(function() {
+    $('#toggleButton').on('click', function() {
+        toggleVisibility();
+    });
 });
 
 // $('.message a').click(function(){
