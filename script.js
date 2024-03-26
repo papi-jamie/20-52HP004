351111964413f42d82c2a94042311f13c114747b
Abdulrahman Jamiu <139684072+papi-jamie@users.noreply.github.com>
2024-03-26 01:13:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ var isHidden = false;
 function toggleVisibility() {
     if (isHidden) {
         $('.text-message').css('opacity', 1);
+        $('button').text('Hide the text above');
     } else {
         $('.text-message').css('opacity', 0);
+        $('button').text('Show the text above');
     }
     isHidden = !isHidden;
 }
