b19ff665ab038fe97795723e06d395fc020ad739
Abdulrahman Jamiu <139684072+papi-jamie@users.noreply.github.com>
2024-03-26 01:09:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-$('.message a').click(function(){
-   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-});
+var isHidden = false;
+
+function toggleVisibility() {
+    if (isHidden) {
+        $('.text-message').css('opacity', 1);
+    } else {
+        $('.text-message').css('opacity', 0);
+    }
+    isHidden = !isHidden;
+}
+
 $('button').on('click', function() {
-   $('p').css('opacity', 0);
+    toggleVisibility();
 });
+
+// $('.message a').click(function(){
+//    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+// });
+// $('button').on('click', function() {
+//    $('p').css('opacity', 0);
+// });
